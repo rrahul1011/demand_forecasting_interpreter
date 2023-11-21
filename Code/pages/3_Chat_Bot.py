eb094857754720e6186ab370fbff08508f3d20f9
rrahul1011 <68853654+rrahul1011@users.noreply.github.com>
2023-11-21 15:32:44 +0530

    updated the req

--- Code/pages/3_Chat_Bot.py
+++ Code/pages/3_Chat_Bot.py
@@ -20,10 +20,10 @@ if "messages" not in st.session_state.keys(): # Initialize the chat message hist
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
-        reader = SimpleDirectoryReader(input_dir="/Users/rahulkushwaha/Desktop/SigmoidGenai/demand_forecasting_interpreter/text_data", recursive=True)
+    with st.spinner(text="Loading and indexing data! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="text_data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the forecasting report  and your job is to answer technical questions. Assume that all questions are related to the report. Keep your answers technical and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
